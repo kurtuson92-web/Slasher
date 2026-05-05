9670f6a20f2c2830b14626274f2e02abf00ef3ca
Kurt Angel D. Uson <kurtuson92@gmail.com>
2026-05-05 15:32:03 +0800

    show attributes added, shop item's attributes shown while buying

--- Gamie.py
+++ Gamie.py
@@ -1,6 +1,6 @@
 # player
 hero_hp = 150
-hero_attack = 20
+hero_attack = 10
 hero_gold = 0
 
 # enemy
@@ -17,9 +17,9 @@ def gold_hero(hero_gold):
     return hero_gold + 5
 
 sample_items = {
-    "Cannon Ball": {"price": 20, "attack": 3},
-    "Heavy Ball": {"price": 75, "attack": 10},
-    "Med Kit":{"price":25,"hp":10}
+    "Cannon Ball +3 atk ": {"price": 20, "attack": 3},
+    "Heavy Ball +10 atk ": {"price": 75, "attack": 10},
+    "Med Kit +5 hp":{"price":25,"hp":5}
 }
 
 inventory = []
@@ -48,7 +48,8 @@ while True:
     print("1. Attack")
     print("2. Farm")
     print("3. Shop")
-    print("4. Run")
+    print("4. Attributes")
+    print("5. Run")
 
     action = input("> ")
 
@@ -68,46 +69,61 @@ while True:
         # SHOP
     elif action == "3":
         print("\n=====SHOP ITEMS=====")
+        print("Gold:", "\033[93m",hero_gold,"\033[0m")
 
         item_list = list(sample_items.items())
 
+        #enumerate items lists
         for i, (item, data) in enumerate(item_list, start=1):
-            print(f"{i}. {item} - {data['price']} gold")
+            print(f"{i}. {item} - \033[93m{data['price']} gold\033[0m")
 
         print("0. Exit")
 
         choice = input("Choose item number: ")
 
+        #leaving shop
         if choice == "0":
-            print("Thanks for Buying!!")
             print("Leaving shop...")
 
         elif choice.isdigit():
             index = int(choice) - 1
 
+            #lenght of items in the shop
             if 0 <= index < len(item_list):
                 item_name, item_data = item_list[index]
 
+                #checks if hero_gold is greater than or equals to item price if true helo_gold will be deducted by the price
                 if hero_gold >= item_data["price"]:
                     hero_gold -= item_data["price"]
                     inventory.append(item_name)
 
+                    #if the item is attack attributes it will add to hero attack
                     if "attack" in item_data:
                         hero_attack += item_data["attack"]
 
+                    #if the item is hp attributes item will update the hp
                     if "hp" in item_data:
                         hero_hp += item_data["hp"]
 
+                    #if item is successfully baught 
                     print(f"You bought {item_name}!")
                 else:
-                    print("Not enough gold!")
+
+                    #if gold is lessthan item price
+                    print("\033[31mNot enough gold!\033[0m")
             else:
                 print("Invalid choice.")
         else:
             print("Enter a number only.")
 
-    # RUN
+
     elif action == "4":
+        print("Your Attack: ",hero_attack)
+        print("Enemy Attack: ",enemy_attack)
+
+    
+    # takbo hanggat may lupa
+    elif action == "5":
         print("You ran away!")
         break
 
